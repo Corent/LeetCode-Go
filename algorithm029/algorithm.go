27fe69b7f3fd43da2f3f0bbc233e2b898b934712
lichen.sven <lichen.sven@bytedance.com>
2024-06-30 21:30:07 +0800

    update

--- algorithm029/algorithm.go
+++ algorithm029/algorithm.go
@@ -1,5 +1,43 @@
 package algorithm029
 
+import "math"
+
 func divide(dividend int, divisor int) int {
-	return 0
+	if divisor == 0 {
+		return math.MinInt
+	}
+	if dividend == math.MinInt && divisor == -1 {
+		return math.MaxInt
+	}
+	if divisor == 1 || dividend == 0 {
+		return dividend
+	}
+	ans, isNegative := 0, (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)
+	dend, sor := int64(dividend), int64(divisor)
+	if dividend < 0 {
+		dend = -dend
+	}
+	if divisor < 0 {
+		sor = -sor
+	}
+	for dend >= sor {
+		digit, tmp := 1, sor<<1
+		for dend > tmp {
+			digit <<= 1
+			tmp <<= 1
+		}
+		ans += digit
+		tmp >>= 1
+		dend -= tmp
+	}
+	if isNegative {
+		ans = -ans
+	}
+	if ans > math.MaxInt32 {
+		ans = math.MaxInt32
+	}
+	if ans < math.MinInt32 {
+		ans = math.MinInt32
+	}
+	return ans
 }
---
+++ algorithm030/algorithm.go
@@ -0,0 +1,5 @@
+package algorithm030
+
+func findSubstring(s string, words []string) []int {
+	return nil
+}
