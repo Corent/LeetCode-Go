8b3ab77fc5818689b47ac1832ba488436bbd30c6
lichen.sven <lichen.sven@bytedance.com>
2024-10-22 13:51:35 +0800

    update

--- algorithm637/algorithm.go
+++ algorithm637/algorithm.go
@@ -7,5 +7,23 @@ type TreeNode struct {
 }
 
 func averageOfLevels(root *TreeNode) []float64 {
-	return nil
+	if root == nil {
+		return nil
+	}
+	queue, rst := []*TreeNode{root}, make([]float64, 0)
+	for len(queue) > 0 {
+		nextQueue, sum := make([]*TreeNode, 0), 0
+		for _, node := range queue {
+			sum += node.Val
+			if node.Left != nil {
+				nextQueue = append(nextQueue, node.Left)
+			}
+			if node.Right != nil {
+				nextQueue = append(nextQueue, node.Right)
+			}
+		}
+		rst = append(rst, float64(sum)/float64(len(queue)))
+		queue = nextQueue
+	}
+	return rst
 }
---
+++ algorithm639/algorithm.go
@@ -0,0 +1,5 @@
+package algorithm639
+
+func numDecodings(s string) int {
+	return 0
+}
