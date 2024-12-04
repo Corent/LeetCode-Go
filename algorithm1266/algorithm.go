d7086b1cb1544644c3df4e7fb953daac0b50624e
lichen.sven <lichen.sven@bytedance.com>
2024-12-05 00:49:45 +0800

    update

--- algorithm1266/algorithm.go
+++ algorithm1266/algorithm.go
@@ -1,5 +1,25 @@
 package algorithm1266
 
 func minTimeToVisitAllPoints(points [][]int) int {
-	return 0
+	rst, x0, x1 := 0, points[0][0], points[0][1]
+	for i := 1; i < len(points); i++ {
+		y0, y1 := points[i][0], points[i][1]
+		rst += maxInt(absInt(x0-y0), absInt(x1-y1))
+		x0, x1 = y0, y1
+	}
+	return rst
+}
+
+func maxInt(m, n int) int {
+	if m > n {
+		return m
+	}
+	return n
+}
+
+func absInt(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
---
+++ algorithm1282/algorithm.go
@@ -0,0 +1,5 @@
+package algorithm1282
+
+func groupThePeople(groupSizes []int) [][]int {
+	return nil
+}
