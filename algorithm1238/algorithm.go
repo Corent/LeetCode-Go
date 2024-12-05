54dc4fc0787166b80398a24beaf39e81c5431c49
lichen.sven <lichen.sven@bytedance.com>
2024-12-05 14:36:04 +0800

    update

---
+++ algorithm1238/algorithm.go
@@ -0,0 +1,5 @@
+package algorithm1238
+
+func circularPermutation(n int, start int) []int {
+	return nil
+}
--- algorithm1282/algorithm.go
+++ algorithm1282/algorithm.go
@@ -1,5 +1,15 @@
 package algorithm1282
 
 func groupThePeople(groupSizes []int) [][]int {
-	return nil
+	groups := make(map[int][]int)
+	for i, size := range groupSizes {
+		groups[size] = append(groups[size], i)
+	}
+	rst := make([][]int, 0)
+	for size, people := range groups {
+		for i := 0; i < len(people); i += size {
+			rst = append(rst, people[i:i+size])
+		}
+	}
+	return rst
 }
