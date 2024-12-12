2a16b2d9def70476de65532429ecb3fa3000516f
lichen.sven <lichen.sven@bytedance.com>
2024-12-12 11:25:11 +0800

    update

--- algorithm1360/algorithm.go
+++ algorithm1360/algorithm.go
@@ -1,5 +1,41 @@
 package algorithm1360
 
+import (
+	"strconv"
+	"strings"
+)
+
 func daysBetweenDates(date1 string, date2 string) int {
-	return 0
+	rst := dateToInt(date1) - dateToInt(date2)
+	if rst < 0 {
+		rst = -rst
+	}
+	return rst
+}
+
+func dateToInt(date string) int {
+	ss := strings.Split(date, "-")
+	if len(ss) != 3 {
+		return 0
+	}
+	year, _ := strconv.Atoi(ss[0])
+	month, _ := strconv.Atoi(ss[1])
+	day, _ := strconv.Atoi(ss[2])
+	rst, months := day-1, []int{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	for month != 0 {
+		month--
+		rst += months[month]
+		if month == 2 && isLeapYear(year) {
+			rst += 1
+		}
+	}
+	rst += 365 * (year - 1971)
+	rst += (year-1)/4 - 1971/4
+	rst -= (year-1)/100 - 1971/100
+	rst += (year-1)/400 - 1971/400
+	return rst
+}
+
+func isLeapYear(year int) bool {
+	return year%400 == 0 || year%100 != 0 && year%4 == 0
 }
---
+++ algorithm1372/algorithm.go
@@ -0,0 +1,11 @@
+package algorithm1372
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func longestZigZag(root *TreeNode) int {
+	return 0
+}
